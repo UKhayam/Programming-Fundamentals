7e2fc1eeed462447f5eb9e001432d2103080f844
Umar13755 <umarkhayyam570@gmail.com>
2024-12-05 17:11:50 +0500

    Q2 added

---
+++ Labs/Lab 03 Solution/Q2.cpp
@@ -0,0 +1,15 @@
+
+
+// Q2 Pseduo Code =>
+
+// Start
+// Declare a variable number.
+// Print "Enter a number:"
+// Input value for number.
+// If number > 50:
+// Print "The number is greater than 50."
+// Else if number == 50:
+// Print "The number is equal to 50."
+// Else:
+// Print "The number is less than 50."
+// End
